d1bf20c8b1ab81bc552fa80af2687fbc61005756
Andrii Lundiak <landike@gmail.com>
2018-05-09 02:00:17 +0200

    Initial commit of code

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# birthday-bot
-Birthday Bot built primary for Skype Bot Platform
+# skype-birthday-bot
+NodeJS based Skype bot to inform chat members about members birthdays.
+
+
+# Tech Stack
+- https://github.com/adaltas/node-csv
+- https://github.com/SheetJS/js-xlsx
---
+++ server.js
@@ -0,0 +1,28 @@
+//
+// Skype Birthday Bot
+//
+require('dotenv').config();
+
+module.exports = function() {
+    var builder = require('botbuilder');
+    var restify = require('restify');
+    var BotBrain = require('./skype-birthday-bot');
+
+    var server = restify.createServer({
+        name: 'skype-birthday-bot',
+        version: '1.0.0'
+    });
+
+    var connector = new builder.ChatConnector({
+        appId: process.env.MICROSOFT_APP_ID,
+        appPassword: process.env.MICROSOFT_APP_PASSWORD
+    });
+
+    var bot = new BotBrain(connector);
+
+    server.post('/api/messages', connector.listen());
+
+    server.listen(process.env.PORT || 3978, function() {
+        console.log('%s listening to %s', server.name, server.url);
+    });
+}
